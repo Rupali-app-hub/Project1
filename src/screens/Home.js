7d58dd9d3c5563915b2e09a84888fcac7333a973
rupali-visualpath <rupalivisualpath@gmail.com>
2024-02-08 12:24:36 +0530

    home screen changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,30 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import VideoPlayer from '../videoPlayer/VideoPlayer'
+import sign from 'jwt-encode';
+import  {jwtDecode}  from "jwt-decode";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import  'core-js/stable/atob';
 
 const Home = () => {
+  const email = AsyncStorage.getItem('EMAIL')
+  const name = AsyncStorage.getItem('NAME')
+  console.log(email, name)
+  const secret = 'secret';
+    const JSONbodyText = {
+        email: email,
+        name: name,
+        empid: 1516239022
+    };
+      const smsbody = sign(JSONbodyText, secret);
+      console.log(smsbody,'smsbody')
+      const decoded = jwtDecode(smsbody);
+      console.log(decoded,'decoded')
   return (
     <SafeAreaView>
+      <View>
+        <Text>User data:</Text>
+      </View>
      <VideoPlayer />
     </SafeAreaView>
   )
